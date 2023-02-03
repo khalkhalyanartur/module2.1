3848eb5958c926f7545aa6819517a52456528d9e
Artur Khalkhalyan <khalkhalyan.fortech@gmail.com>
2023-02-03 12:07:28 +0300

    feat: create task2.7

---
+++ 2.7.js
@@ -0,0 +1,23 @@
+/*
+Дана строка. Нужно написать функцию, которая возвращает значение true, если строка является палиндромом, или false если нет.
+Input: "testset"
+Output: true
+Input: "abbcsa"
+Output: false
+*/
+const  text = "топот";
+
+const isPolindrom = (string) => {
+  let result = true;
+
+  for (let i = 0; i < (string.length) / 2; i++) {
+    if (string[i] != string[string.length-i-1]) {
+      result = false;
+      return result;
+    }
+  }
+  
+  return result;
+}
+
+console.log(isPolindrom(text));
