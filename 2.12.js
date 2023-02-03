5bbcc5f689452618010ba859f84078a3b5851095
Artur Khalkhalyan <khalkhalyan.fortech@gmail.com>
2023-02-03 12:14:08 +0300

    feat: create task2.12

---
+++ 2.12.js
@@ -0,0 +1,18 @@
+/*
+Реализуйте функцию, которая принимает на вход объект типа { "dog": 6, "cat": 11 } и возвращает массив пар.
+Input: { "dog": 6, "cat": 11 }
+Output: [ ["dog", 6], ["cat", 11] ]
+*/
+
+const convertObjectToArray = (obj) => {
+  const result = [];
+  let count = 0;
+  for (let key in obj) {
+    result[count] = [key, obj[key]];
+    count++;
+  }
+
+  return result;
+} 
+
+console.log(convertObjectToArray({ "dog": 6, "cat": 11 }));
