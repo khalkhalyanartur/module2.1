76cfd03cfc0c1261035ec1087f22b87f3f5c78ad
khalkhalyanartur <123370201+khalkhalyanartur@users.noreply.github.com>
2023-02-07 18:54:51 +0300

    Update 2.2.js

--- 2.2.js
+++ 2.2.js
@@ -6,30 +6,26 @@ Output: [1]
 */
 
 const searchRepeat = (...arguments) => {
-  let result = [];
+  const result = [];
   let countArray = 0;
 
-  //searchRepeat([3, 6, 1, 8, 3, 6, 3, 6, 3, 6], [1, 4, 2, 4], [6, 3, 2, 8, 1])
   for (let i = 0; i < arguments[0].length; i++) {
     countArray = 0;
-    console.log(arguments[0][i]);
-    
+
     for (let j = 1; j < arguments.length; j++) {
       for (let k = 0; k < arguments[j].length; k++) {
-        console.log(" ",arguments[j][k]);
-        if (arguments[0][i] == arguments[j][k]) {
-          console.log("da");
+        if (arguments[0][i] === arguments[j][k]) {
           countArray ++;
           break;
         }
       }
     }
-    if (countArray == (arguments.length-1)) {
+    if (countArray === (arguments.length-1)) {
       result.push(arguments[0][i]);
     }
   }
-  return result
 
+  return result
 }
 
 console.log(searchRepeat([3, 6, 1, 8, 3, 6, 3, 6, 3, 6, 4], [ 8, 3, 2, 4], [6, 3, 2, 8]));
