d1130f186015e10c6fae85a0b80cbdba395097da
Artur <khalkhalyan@gmail.com>
2023-02-11 23:04:39 +0300

    edit

--- 2.2.js
+++ 2.2.js
@@ -9,27 +9,23 @@ const searchRepeat = (...arguments) => {
   let result = [];
   let countArray = 0;
 
-  //searchRepeat([3, 6, 1, 8, 3, 6, 3, 6, 3, 6], [1, 4, 2, 4], [6, 3, 2, 8, 1])
   for (let i = 0; i < arguments[0].length; i++) {
     countArray = 0;
-    console.log(arguments[0][i]);
     
     for (let j = 1; j < arguments.length; j++) {
       for (let k = 0; k < arguments[j].length; k++) {
-        console.log(" ",arguments[j][k]);
         if (arguments[0][i] == arguments[j][k]) {
-          console.log("da");
           countArray ++;
           break;
         }
       }
     }
-    if (countArray == (arguments.length-1)) {
+    if (countArray === (arguments.length - 1)) {
       result.push(arguments[0][i]);
     }
   }
-  return result
 
+  return result
 }
 
-console.log(searchRepeat([3, 6, 1, 8, 3, 6, 3, 6, 3, 6, 4], [ 8, 3, 2, 4], [6, 3, 2, 8]));
+console.log(searchRepeat([3, 6, 1, 8, 3, 6, 3, 6, 3, 6], [1, 4, 2, 4], [6, 3, 2, 8, 1]));
