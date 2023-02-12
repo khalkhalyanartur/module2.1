dac2fb9908b9f5a86c37808ab5cf127e030ebcca
Artur <khalkhalyan@gmail.com>
2023-02-12 22:26:54 +0300

    edit 2.14

--- 2.14.js
+++ 2.14.js
@@ -1,5 +1,6 @@
 /*
-Даны две строки. Напишите функцию, которая определит, содержится ли меньшая по длине строка в большей.
+Даны две строки. Напишите функцию, которая определит, содержится ли 
+меньшая по длине строка в большей.
 
 Input: ("text education part 2", "text")
 Output: true
@@ -7,18 +8,28 @@ Output: true
 Input: ("text education part 2", "test")
 Output: false
 */
-const string1 = "text education part 2";
-const string2 = "part";
+const string2 = "text education part 2";
+const string1 = "part";
 
-const isContains = (string, subString) => {
+const isContainsString = (string, subString) => {
+  let tempString = '';
+
+  if (string.length < subString.length) {
+    tempString = subString;
+    subString = string;
+    string = tempString;
+  }
+
 
   for (let i = 0; i < string.length; i++) {
-    if (string[i] == subString[0]) {
+    if (string[i] === subString[0]) {
       for (let j = 0; j < subString.length; j++) {
-        if (string[i+j] != subString[j]) {
+
+        if (string[i + j] != subString[j]) {
           break;
         }
-        if (string[i+j] === subString[j] && j == subString.length - 1) {
+
+        if (string[i + j] === subString[j] && j == subString.length - 1) {
           return true;
         }
       }
@@ -28,4 +39,4 @@ const isContains = (string, subString) => {
   return false;
 }  
 
-console.log(isContains(string1,string2));
+console.log(isContainsString(string1, string2));
